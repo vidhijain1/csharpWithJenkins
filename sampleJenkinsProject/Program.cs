9b543d8fcc64c10c0be04c67d63730936cbbc530
vidhijain1 <76040976+vidhijain1@users.noreply.github.com>
2020-12-17 13:21:31 +0530

    Program.cs
    error build

--- sampleJenkinsProject/Program.cs
+++ sampleJenkinsProject/Program.cs
@@ -11,6 +11,7 @@ namespace sampleJenkinsProject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello");
+            Console.WriteLine("Hello1")
         }
     }
 }
